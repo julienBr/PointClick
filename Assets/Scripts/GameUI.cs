7ca11ed4fa7e141aa71a10f3145bd70363abf14b
julienBr <julien.bringard@outlook.fr>
2022-12-30 18:30:46 +0100

    Update Scene

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,30 +1,26 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
-    private static GameUI _gameUI;
     [SerializeField] private TMP_Text cluesFoundText;
-    
-    /*private void Awake()
-    {
-        if (_gameUI != null && _gameUI != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        _gameUI = this;
-        DontDestroyOnLoad(this);
-    }*/
+    [SerializeField] private Image clue1;
+    [SerializeField] private Image clue2;
+    [SerializeField] private Image clue3;
+    [SerializeField] private Image clue4;
+    [SerializeField] private Image clue5;
+    [SerializeField] private Image clue6;
+    [SerializeField] private Image clue7;
 
     private void Update()
     {
         UpdateClues();
+        if (PauseMenu.returnToMainMenu) PlayerController.cluesFound = 0;
     }
 
     private void UpdateClues()
     {
-        if (PauseMenu.returnToMainMenu) PlayerController.cluesFound = 0;
         cluesFoundText.text = "Clues Found : " + PlayerController.cluesFound + " / 7";
     }
 }
