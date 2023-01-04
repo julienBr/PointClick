215e11ba629b053b72d935b63621213cb872a512
julienBr <julien.bringard@outlook.fr>
2023-01-04 10:25:09 +0100

    Update GameUI

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,18 +1,10 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text timer;
     [SerializeField] private TMP_Text cluesFoundText;
-    [SerializeField] private Image clue1;
-    [SerializeField] private Image clue2;
-    [SerializeField] private Image clue3;
-    [SerializeField] private Image clue4;
-    [SerializeField] private Image clue5;
-    [SerializeField] private Image clue6;
-    [SerializeField] private Image clue7;
 
     private void Update()
     {
